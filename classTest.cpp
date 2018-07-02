167da2c1e5dfd71f531209eefdfa746338747063
Nahum Royer <nahum.royer@wdc.com>
2018-07-02 14:10:07 -0700

    "Updated to explore classes over multiple files"

--- classTest.cpp
+++ classTest.cpp
@@ -1,26 +1,53 @@
 // Learning object oriented programming and sysC modeling with TLM
 #include <iostream>
 #include <stdio.h>
+#include <string>
 using namespace std;
 
 class TestClass
 {
     public:
+        TestClass(string z)              // This is the constructor
+        {
+            setName(z);
+        }
+
         void quotePrint()
         {
             printf("Quoted words via printf\n");
         }
+
         void quoteCout()
         {
             cout << "Quoted words via cout" << endl;
         }
+
+        void setName(string x)    // Setting name 
+        {
+            name = x;
+        }
+
+        string getName()         // Getting Name
+        {
+            return name;
+        }
+
+
+    private:
+        string name;
+
 };
 
 int main()
 {
-    TestClass tc;
-    tc.quotePrint();
-    tc.quoteCout();
+    TestClass tc("Constructing Crazy Constructors");  // Adding a parameter to the constructor
+    tc.quotePrint();                                  // Calling the TestClass quotePrint funct
+    tc.quoteCout();                                   // Calling the TestClass quoteCout funct
+    cout << tc.getName() << endl;                     // Testing constructor call
+    tc.setName("Setting The Name");                   // setting new name
+    cout << tc.getName() << endl;                     // printing new name
+    TestClass tc2("tc2 Constructor");
+    cout << tc2.getName() << endl;
     return 0;
 }
 
